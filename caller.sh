0b227391a53d3a889de2bbbb8dd37e2156cf002e
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2016-10-20 00:21:56 +0530

    * caller.sh: dumb caller.

---
+++ caller.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Created: Thursday, October  6 2016
+
+# ./nclrunner.py -varname "tsfc prate ts" -expmt "ref ftpc"  mean.ncl
+# ./nclrunner.py -varname "tsfc prate ts"                    mean_anom.ncl
+# ./nclrunner.py -varname "tsfc prate ts" -expmt "ref ftpc"  mean_mon.ncl
+# ./nclrunner.py -varname "tsfc prate ts"                    mean_mon_anom.ncl
+
+# ./nclrunner.py -varname "tsfc prate ts" -expmt "ref ftpc" std.ncl
+# ./nclrunner.py -varname "tsfc prate ts"                   std_anom.ncl
+# ./nclrunner.py -varname "tsfc prate ts" -expmt "ref ftpc" std_mon.ncl
+# ./nclrunner.py -varname "tsfc prate ts"                   std_mon_anom.ncl
+
+# ./nclrunner.py -varname "tsfc prate ts"  mean_ts.ncl
