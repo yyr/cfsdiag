18143159a103823f9ec32979bafd07a4465d2629
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2016-11-11 07:32:09 +0530

    * caller.sh: update.

--- caller.sh
+++ caller.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 # Created: Thursday, October  6 2016
 
-# ./nclrunner.py -varname "tsfc prate ts" -expmt "ref ftpc"  mean.ncl
-# ./nclrunner.py -varname "tsfc prate ts"                    mean_anom.ncl
-# ./nclrunner.py -varname "tsfc prate ts" -expmt "ref ftpc"  mean_mon.ncl
-# ./nclrunner.py -varname "tsfc prate ts"                    mean_mon_anom.ncl
+# ./nclrunner.py -varname "prate ts" -expmt "ref ftpc"  mean.ncl
+# ./nclrunner.py -varname "prate ts"                    mean_anom.ncl
+# ./nclrunner.py -varname "prate ts" -expmt "ref ftpc"  mean_mon.ncl
+# ./nclrunner.py -varname "prate ts"                    mean_mon_anom.ncl
 
-# ./nclrunner.py -varname "tsfc prate ts" -expmt "ref ftpc" std.ncl
-# ./nclrunner.py -varname "tsfc prate ts"                   std_anom.ncl
-# ./nclrunner.py -varname "tsfc prate ts" -expmt "ref ftpc" std_mon.ncl
-# ./nclrunner.py -varname "tsfc prate ts"                   std_mon_anom.ncl
+# ./nclrunner.py -varname "prate ts" -expmt "ref ftpc" std.ncl
+# ./nclrunner.py -varname "prate ts"                   std_anom.ncl
+# ./nclrunner.py -varname "prate ts" -expmt "ref ftpc" std_mon.ncl
+# ./nclrunner.py -varname "prate ts"                   std_mon_anom.ncl
 
-# ./nclrunner.py -varname "tsfc prate ts"  mean_ts.ncl
+# ./nclrunner.py -varname "prate ts"  mean_ts.ncl
+
+# ./nclrunner.py -reg "nino wiod eiod"  anual_mean_sst.ncl
+# ./nclrunner.py -reg "nino wiod eiod"  interanual_vari_sst.ncl
