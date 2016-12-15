5b51ab018466d0c3a48982ec939163105ac67151
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2016-12-15 17:57:47 +0530

    cleanup; remap_landmask.sh

--- remap_landmask.sh
+++ remap_landmask.sh
@@ -1,4 +1,14 @@
 # intial  cfs2_global_slmask.t126.nc has gaussian grid with 385x191
 # convert it 360x181 latlon grid.
 # check landmask.coords file.
-cdo remapbil,landmask.coords cfs2_global_slmask.t126.nc landmask.nc
+cat <<- EOF > /tmp/landmask
+gridtype = lonlat
+xsize    = 360
+ysize    = 181
+xfirst   = 0
+xinc     = 1
+yfirst   = 90
+yinc     = -1
+EOF
+
+cdo remapbil,/tmp/landmask cfs2_global_slmask.t126.nc landmask.nc
