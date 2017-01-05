f68820904b9e302090bc411b881034f3c4ebdcf2
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2017-01-05 23:55:37 +0530

    * caller.sh: update with all scripts.

--- caller.sh
+++ caller.sh
@@ -2,16 +2,18 @@
 # Created: Thursday, October  6 2016
 
 # ./nclrunner.py -varname "prate ts" -expmt "ref ftpc obs"  mean.ncl
-# ./nclrunner.py -varname "prate ts"                        mean_anom.ncl
 # ./nclrunner.py -varname "prate ts" -expmt "ref ftpc obs"  mean_mon.ncl
-# ./nclrunner.py -varname "prate ts"                        mean_mon_anom.ncl
 
-# ./nclrunner.py -varname "prate ts" -expmt "ref ftpc obs" std.ncl
-# ./nclrunner.py -varname "prate ts"                       std_anom.ncl
-# ./nclrunner.py -varname "prate ts" -expmt "ref ftpc obs" std_mon.ncl
-# ./nclrunner.py -varname "prate ts"                       std_mon_anom.ncl
+# ./nclrunner.py -varname "prate ts" -expmt "ref ftpc obs"  std.ncl
+# ./nclrunner.py -varname "prate ts" -expmt "ref ftpc obs"  std_mon.ncl
+
+# ./nclrunner.py -varname "prate ts"                         mean_anom.ncl
+# ./nclrunner.py -varname "prate ts"                         mean_mon_anom.ncl
+
+# ./nclrunner.py -varname "prate ts"                         std_anom.ncl
+# ./nclrunner.py -varname "prate ts"                         std_mon_anom.ncl
 
 # ./nclrunner.py -varname "prate ts"  mean_ts.ncl
 
-# ./nclrunner.py -reg "nino wiod eiod"  anual_mean_sst.ncl
+./nclrunner.py -reg "nino wiod eiod"  anual_mean_sst.ncl
 # ./nclrunner.py -reg "nino wiod eiod"  variablity_sst.ncl
